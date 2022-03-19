0e0d1d148dd737edad2637b7975cba79f1bb93ea
Sahaj Patel <74474849+sah4jpatel@users.noreply.github.com>
2022-03-19 17:02:35 -0400

    integrated buffer to text detection

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+import io
 from text_detection import detect_text
+from typing import Iterator
+from PIL import Image
 
-detect_text("src/tests/data/unknown.png")
+
+def pathToIter(inStr : str) -> Iterator[Image.Image]:
+    im = Image.open(inStr) 
+    yield im
+    
+out = detect_text(pathToIter("src/tests/data/probability.1130400.jpg"))
+print(out[0])
+
+#detect_text("src/tests/data/unknown.png")
--- src/text_detection.py
+++ src/text_detection.py
@@ -1,35 +1,52 @@
 import io
-
+from tabnanny import verbose
 from google.cloud import vision
+from PIL import Image
+from typing import Iterator, cast
+from numpy import dtype
+
+
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_file(
+    'cloud_credentials.json')
 
 
 # FIXME: output should be returned, not printed
-def detect_text(path):
+def detect_text(im: Iterator[Image.Image]):
     """Detects text in the file."""
 
-    client = vision.ImageAnnotatorClient()
+    client = vision.ImageAnnotatorClient(credentials=credentials)
 
-    with io.open(path, "rb") as image_file:
-        content = image_file.read()
+    for image in im:
+        temp = io.BytesIO()
+        image.save(temp, format='PNG')
+        content = temp.getvalue()
 
-    image = vision.Image(content=content)
+        image = vision.Image(content=content)
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print("Texts:")
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+        print("Texts:")
 
-        vertices = [
-            "({},{})".format(vertex.x, vertex.y)
-            for vertex in text.bounding_poly.vertices
-        ]
+        out = []
+        i = 0
 
-        print("bounds: {}".format(",".join(vertices)))
+        for text in texts:
+            #print('\n"{}"'.format(text.description))
+            out.append(text.description)
 
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
+            vertices = [
+                "({},{})".format(vertex.x, vertex.y)
+                for vertex in text.bounding_poly.vertices
+            ]
+
+
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
+        
+        return out
